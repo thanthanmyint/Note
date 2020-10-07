dc4331f591816c41e812979a23cf36024b4de331
thanthanmyint <ttmyint@isgm2.com>
2020-10-07 15:06:17 +0630

    note app start

--- Note/Note/MainPage.xaml.cs
+++ Note/Note/MainPage.xaml.cs
@@ -5,14 +5,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.IO;
 
 namespace Note
 {
     public partial class MainPage : ContentPage
     {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notes.txt");
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(_fileName))
+            {
+                editor.Text = File.ReadAllText(_fileName);
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_fileName, editor.Text);
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            if (File.Exists(_fileName))
+            {
+                File.Delete(_fileName);
+            }
+            editor.Text = string.Empty;
         }
     }
 }
--- Note/Note/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ Note/Note/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Note {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Editor editor;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            editor = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Editor>(this, "editor");
         }
     }
 }
